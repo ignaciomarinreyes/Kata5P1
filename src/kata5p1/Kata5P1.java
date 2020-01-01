0fba60e5a242e6a93c718976931311929f438e1e
ignaciomarinreyes <ignaciomarinreyes@192.168.0.11>
2020-01-01 17:18:36 +0000

    Kata5P1 version2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,14 +1,8 @@
 package kata5p1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Kata5P1 {
-
+    
     public static void main(String[] args) {
-
-        SelectApp app = new SelectApp();
-        app.selectAll();
+        CrearTabla.createNewTable();
     }
 }
